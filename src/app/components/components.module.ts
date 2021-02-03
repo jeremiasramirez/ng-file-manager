ec028acb09e6be402fcb920927f02b238ba14d2a
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-03 19:21:09 -0400

    Agregando carpeta de icon + componente de banner main

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { StartBannerComponent } from './start-banner/start-banner.component';
 import {MatButtonModule} from "@angular/material/button";
 import { LoadingPageComponent } from './loading-page/loading-page.component';
+import { BannerUserComponent } from './banner-user/banner-user.component';
 
 
 @NgModule({
   declarations: [     
    
-  StartBannerComponent, LoadingPageComponent ,
+  StartBannerComponent, LoadingPageComponent, BannerUserComponent ,
  
 ],
   imports: [
