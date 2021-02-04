f47cca81514b3b2d8dbb68ba696ecc5259526555
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-03 23:03:31 -0400

    mejorando animacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceHelpers } from './services/service.helpers';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'file-manager';
+  constructor(private route:ServiceHelpers){
+    this.route.to("/start",0)
+  }
 }
