1eff4da3f4038bec312ee79df297108173d87678
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-04 23:30:22 -0400

    mejorando animaciones + agregado nuevo item

--- src/app/services/service.list-storage.ts
+++ src/app/services/service.list-storage.ts
@@ -13,10 +13,12 @@ export class ListStorage{
 
  list(): any[] {
     return [
+        {name: 'Local', percentage: '98%', img: "../../../assets/images/folder.svg"},
         {name: 'iCloud', percentage: '64%', img: "../../../assets/images/icloud.svg"},
         {name: 'Google Drive', percentage: '46%', img: "../../../assets/images/google-drive.svg"},
         {name: 'Dropbox', percentage: '80%', img:"../../../assets/images/dropbox.svg"},
         {name: 'Mega', percentage: '10%', img: "../../../assets/images/mega.svg"}
+        
     ];
 }
 
